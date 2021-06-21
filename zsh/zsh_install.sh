c5c21640e403924283c4521934dabcb585d23bee
Brian Thompson <brian@hashvault.io>
2021-06-20 21:06:39 -0500

    Setup zsh stuff

--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo apt install -y zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo apt install -y exa
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

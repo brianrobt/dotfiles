6586f20e4d29d728258931fc037889231f24dc97
Thompson, Brian <brian.thompson@mastercard.com>
2021-07-12 06:13:58 -0500

    Rename findr to findr.sh

--- debian/usr/local/bin/findr.sh
+++ debian/usr/local/bin/findr.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
 find . -type f -name "*.md" | sort -R | head -n 1
--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y zsh zsh-autosuggestions zsh-syntax-highlighting
-sudo apt install -y exa
+if [ -f /usr/bin/apt ]; then
+  sudo apt update
+  sudo apt install -y zsh zsh-autosuggestions zsh-syntax-highlighting
+  sudo apt install -y exa
+else
+  sudo yum install -y zsh zsh-autosuggestions zsh-syntax-highlighting
+fi
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

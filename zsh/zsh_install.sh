2deab9707392fa990e9cce933c8ea886f845c5a5
Thompson, Brian <git@hashvault.io>
2021-07-25 18:51:00 -0500

    Add echo for post-install powerlevel10k

--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -13,4 +13,4 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 echo "Install powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
+echo "Change the value of ZSH_THEME to \"powerlevel10k/powerlevel10k\" to use the theme."

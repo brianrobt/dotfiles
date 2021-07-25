24f3dce8964c97dae6968efe07b96f00dd651322
Thompson, Brian <git@hashvault.io>
2021-07-25 18:49:28 -0500

    Add powerlevel10k installation

--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo "Installing zsh packages."
 if [ -f /usr/bin/apt ]; then
   sudo apt update
   sudo apt install -y zsh zsh-autosuggestions zsh-syntax-highlighting
-  sudo apt install -y exa
 else
   sudo yum install -y zsh zsh-autosuggestions zsh-syntax-highlighting
 fi
 
+echo "Installing oh-my-zsh."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Install powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+

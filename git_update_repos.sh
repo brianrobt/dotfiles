38ca8c3aa956dafd430ecff8e3e1395a236203f5
Thompson, Brian <foss.systems@icloud.com>
2021-12-11 04:38:55 -0600

    fix linting errors

--- git_update_repos.sh
+++ git_update_repos.sh
@@ -2,20 +2,19 @@
 
 workspace="workspace"
 
-for dir in $HOME/$workspace/*; do
+for dir in "$HOME/$workspace"/*; do
   echo "Changing dirs to $dir"
-  cd $dir
-  git fetch --all
+  cd "$dir" || return
+  git fetch
   git stash
   branches=$(git branch -a)
-  for branch in branches; do
+  for branch in $branches; do
     if [ "$branch" = "master" ] || [ "$branch" = "main" ] || [ "$branch" = "trunk" ]; then
-      git checkout $branch
-      git pull upstream $branch
-      git push origin $branch
+      git checkout "$branch"
+      git merge upstream "$branch"
+      git push origin "$branch"
       break
     fi
   done
   cd ..
 done
-

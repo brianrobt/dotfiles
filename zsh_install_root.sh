0f4494e622d8e2bca7e1d5a655e026c1b5b7dd8c
Thompson, Brian <brt9023@gmail.com>
2022-03-24 02:13:54 -0500

    Reorganize files

--- zsh_install_root.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Installing oh-my-zsh."
-sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-echo "Install powerlevel10k"
-sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.oh-my-zsh/custom/themes/powerlevel10k
-echo "Change the value of ZSH_THEME to \"powerlevel10k/powerlevel10k\" to use the theme."

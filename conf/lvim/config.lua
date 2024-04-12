baf74530f54cbef9cf0f56e5a752f604fc6c2863
Thompson, Brian <brianrobt@pm.me>
2024-04-12 11:51:40 -0500

    conf(lvim): better diagnostics; lua lsp

--- conf/lvim/config.lua
+++ conf/lvim/config.lua
@@ -4,6 +4,47 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
+----
+-- User plugins
+----
 lvim.plugins = {
   { "tpope/vim-sleuth" },
 }
+
+----
+-- Vim diagnostics
+----
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+})
+
+-- Diagnostic messages will appear as a float
+vim.cmd([[au CursorHold * lua vim.diagnostic.open_float(0,{scope = "cursor"})]])
+
+----
+-- LVim builtins
+----
+lvim.builtin.treesitter.highlight.enable = true
+
+lvim.builtin.treesitter.ensure_installed = {
+    "bash",
+    "c",
+    "css",
+    "java",
+    "javascript",
+    "json",
+    "latex",
+    "lua",
+    "python",
+    "rust",
+    "typescript",
+    "tsx",
+    "yaml",
+}
+
+----
+-- LSP servers
+----
+require("lvim.lsp.manager").setup("marksman")
+require("lvim.lsp.manager").setup("lua_ls")
